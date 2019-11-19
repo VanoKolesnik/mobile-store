30aa93d95a82d5b758c1f642ef16d7e78b1e9bcc
VanoKolesnik <kolesnikivan1002@gmail.com>
2019-11-19 14:49:18 +0200

    Change server port.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
-const port = process.env.port || 1337
+const port = process.env.PORT || 1337
 const DIST_DIR = path.join(__dirname, "../build")
 const PAGE = (file) => {
 	return path.join(DIST_DIR, "/pages/", file)
